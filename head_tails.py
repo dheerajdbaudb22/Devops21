96bd6f4fb6b4c3783e5f2c61742ff8f318344ef8
Dheeraj <dheerajdbaudb@gmail.com>
2021-02-06 21:49:27 +0530

    new line

--- head_tails.py
+++ head_tails.py
@@ -7,5 +7,5 @@ num_items = len(names)
 random_choice = random.randint(0, num_items - 1)
 person_to_pay = names[random_choice]
 print(person_to_pay + "  is going to pay bill for today. ")
-
+prrint(person_to_pay)
 
